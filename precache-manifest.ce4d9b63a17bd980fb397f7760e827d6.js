48a477ffe09e98a4197c7c258ea4c7dd8b7d2e1a
eduoblys <eduoblys@gmail.com>
2019-11-28 01:19:22 +0100

    Updates

---
+++ precache-manifest.ce4d9b63a17bd980fb397f7760e827d6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07824a5a4c0284521288bfd9d71c20ef",
+    "url": "/myweb/index.html"
+  },
+  {
+    "revision": "c9b40f69949dd4ebf6b5",
+    "url": "/myweb/static/css/main.6de4fe76.chunk.css"
+  },
+  {
+    "revision": "f1a2f0298254082b86d1",
+    "url": "/myweb/static/js/2.0b4bdc93.chunk.js"
+  },
+  {
+    "revision": "c9b40f69949dd4ebf6b5",
+    "url": "/myweb/static/js/main.9eb4d625.chunk.js"
+  },
+  {
+    "revision": "0c7db3090636f25bc5e8",
+    "url": "/myweb/static/js/runtime-main.5d84842d.js"
+  }
+]);
