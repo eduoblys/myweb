96467964b6d749a671bcd82d575d0e50e4a3305a
eduoblys <eduoblys@gmail.com>
2019-12-13 06:59:09 +0100

    still doesnt...

--- src/components/About.js
+++ src/components/About.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Home = props => (
-    <div className="text-field">About adfafdfafaaaaaaaaaaaage</div>
+    <div className="text-field">About ge</div>
 );
     
 export default Home;
