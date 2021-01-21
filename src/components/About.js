8ddabc751d6419ca86ed36b35aa0a8b74595ca2e
eduoblys <eduoblys@gmail.com>
2021-01-21 21:44:15 +0100

    gh pages fi4

--- src/components/About.js
+++ src/components/About.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 const Home = props => (
     <div className="text-field">About me!</div>
+    <div className="text-field">About you!</div>
 );
     
 export default Home;
