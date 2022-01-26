0b0cf5e2031f4d269f2056e3eff564276cc32766
eduoblys <eduoblys@gmail.com>
2022-01-26 11:40:57 +0100

    working with CSS

--- src/components/About.js
+++ src/components/About.js
@@ -1,8 +1,15 @@
 import React from 'react';
+import Github from '../assets/github.png'
+import Linkedin from '../assets/linked.png'
+import Email from '../assets/email.png'
 
 const Home = props => (
     <div>
         <div className="text-field">About you!</div>
+        <div className="text-field">Addasdasdsdadasd</div>
+        <a className="social_links" href='https://github.com/eduoblys'><img src={Github}/><div className="text-field-small">GitHub</div></a><br/>
+        <a className="social_links" href='https://www.linkedin.com/in/egidijus-duoblys/'><img src={Linkedin} /><div className="text-field-small">LinkedIn</div></a><br/>
+        <a className="social_links" href='mailto:eduoblys@gmail.com'><img src={Email} /><div className="text-field-small">E-Mail</div></a><br/>
     </div>
 );
     
--- src/components/Toolbar/Toolbar.js
+++ src/components/Toolbar/Toolbar.js
@@ -4,12 +4,12 @@ import { Link } from 'react-router-dom';
 const toolbar = props => (
     <header className="toolbar">
         <nav className="toolbar__navigation">
-            <div className="toolbar__logo"><Link to="/" >LOGO</Link></div>
+            <div className="toolbar__logo"><Link to="/" >Egidijus Duoblys</Link></div>
             <div className="spacer"></div>
             <div className="toolbar_navigation-items">
                 <ul>
-                    <li><Link to="/about" >About!!!</Link></li>
-                    <li><Link to="/" >Landing</Link></li>                    
+                    <li><Link to="/about" >About Me</Link></li>
+                    <li><Link to="/" >L</Link></li>                    
                     <li><Link to="/projects" >Projects</Link></li>                    
                 </ul>
             </div>
