9fb49b75459e1e52988f0223d09a26971bbc7cdc
eduoblys <eduoblys@gmail.com>
2022-01-26 14:10:42 +0100

    fixing dependancies package.json

--- src/components/About.js
+++ src/components/About.js
@@ -5,11 +5,13 @@ import Email from '../assets/email.png'
 
 const Home = props => (
     <div>
-        <div className="text-field">About you!</div>
-        <div className="text-field">Addasdasdsdadasd</div>
-        <a className="social_links" href='https://github.com/eduoblys'><img src={Github}/><div className="text-field-small">GitHub</div></a><br/>
-        <a className="social_links" href='https://www.linkedin.com/in/egidijus-duoblys/'><img src={Linkedin} /><div className="text-field-small">LinkedIn</div></a><br/>
-        <a className="social_links" href='mailto:eduoblys@gmail.com'><img src={Email} /><div className="text-field-small">E-Mail</div></a><br/>
+        <div className="text-field">About you! short summary</div>
+        <div className="text-field">Non-technical skills: hobbies blah blah</div>
+        <div className="links">
+            <a className="social_links" href='https://github.com/eduoblys'><img src={Github}/><div className="text-field-small">GitHub</div></a><br/>
+            <a className="social_links" href='https://www.linkedin.com/in/egidijus-duoblys/'><img src={Linkedin} /><div className="text-field-small">LinkedIn</div></a><br/>
+            <a className="social_links" href='mailto:eduoblys@gmail.com'><img src={Email} /><div className="text-field-small">E-Mail</div></a><br/>
+        </div>
     </div>
 );
     
